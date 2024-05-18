78c9a7a1f3d127dd2db62f1463baba1f206028f0
Vishal Pant <vishal.pant1@gmail.com>
2024-05-18 13:09:37 +0530

    update game logic in JS - currently pending

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function getHumanChoice() {
         return c;
       }
     }
-    alert("Invalid Input. Try again.");
+    console.log("Invalid Input. Try again.");
   }
 }
 
@@ -30,39 +30,39 @@ function playGame() {
   // The playRound() function is a nested function inside playGame(). This contains the actual logic behind the winner/loser based on their respective choices.
   function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-      alert(`You both chose ${humanChoice}, so it's a tie.`);
+      console.log(`You both chose ${humanChoice}, so it's a tie.`);
     } else if (
       (humanChoice === "rock" && computerChoice === "scissors") ||
       (humanChoice === "paper" && computerChoice === "rock") ||
       (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-      alert(
+      console.log(
         `You chose ${humanChoice}. Computer chose ${computerChoice}. You win.`
       );
       humanScore++;
     } else {
-      alert(
+      console.log(
         `You chose ${humanChoice}. Computer chose ${computerChoice}. You lose.`
       );
       computerScore++;
     }
   }
 
-  for (let i = 1; i <= 5; i++) {
-    alert(`Round ${i}. Get Ready!`);
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+  // for (let i = 1; i <= 5; i++) {
+  //   console.log(`Round ${i}. Get Ready!`);
+  //   const humanSelection = getHumanChoice();
+  //   const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
-    alert(`Your score: ${humanScore}. Computer score: ${computerScore}.`);
-  }
-  if (humanScore > computerScore) {
-    alert("You Won the game!");
-  } else if (humanScore < computerScore) {
-    alert("You lost the game!!");
-  } else {
-    alert("Too bad, the game was a tie...");
-  }
+  //   playRound(humanSelection, computerSelection);
+  //   console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`);
+  // }
+  // if (humanScore > computerScore) {
+  //   console.log("You Won the game!");
+  // } else if (humanScore < computerScore) {
+  //   console.log("You lost the game!!");
+  // } else {
+  //   console.log("Too bad, the game was a tie...");
+  // }
 }
 
 playGame();
